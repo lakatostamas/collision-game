3a6be78640a6276ce79e57c29dbaa823413e61cc
tamaslakatos <tamas.lakatos@hu.ibm.com>
2018-03-07 19:46:20 +0100

    chore(webpack): add default config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: './src/index.html', to: '.' }
+    ])
+  ]
+};
+
