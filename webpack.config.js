70920985502aaa3dcd36c83d3f9e861d22331fe9
tamaslakatos <tamas.lakatos@hu.ibm.com>
2018-04-25 17:56:44 +0200

    chore(build): update build process

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
