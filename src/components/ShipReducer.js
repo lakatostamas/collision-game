48cdc9bfe0c8c4993b918203d9043ae9c848b087
lakatostamas <mail.tamaslakatos@gmail.com>
2018-05-14 21:59:03 +0200

    stlye(ship): fix eslint errors

--- src/components/ShipReducer.js
+++ src/components/ShipReducer.js
@@ -1,35 +1,42 @@
 export default function shipReducer({ keyCode, currentPosition }) {
   const speed = 0.08;
   const inertia = 0.99;
-  let { x, y, rotation, velocityX, velocityY } = currentPosition;
+  const {
+    x,
+    y,
+    rotation,
+    velocityX,
+    velocityY,
+  } = currentPosition;
 
-  switch(keyCode) {
+  switch (keyCode) {
     case 37:
       return Object.assign({}, currentPosition, {
-        x: x + velocityX / 5,
-        y: y + velocityY / 5,
+        x: x + (velocityX / 5),
+        y: y + (velocityY / 5),
         velocityX: velocityX * inertia,
         velocityY: velocityY * inertia,
         rotation: rotation - 4,
       });
     case 39:
       return Object.assign({}, currentPosition, {
-        x: x + velocityX / 5,
-        y: y + velocityY / 5,
+        x: x + (velocityX / 5),
+        y: y + (velocityY / 5),
         velocityX: velocityX * inertia,
         velocityY: velocityY * inertia,
         rotation: rotation + 4,
       });
-    case 38:
-      velocityX += Math.sin(-rotation * Math.PI / 180) * speed;
-      velocityY += Math.cos(-rotation * Math.PI / 180) * speed
+    case 38: {
+      const increasedVelocityX = velocityX + (Math.sin((-rotation * Math.PI) / 180) * speed);
+      const increasedVelocityY = velocityY + (Math.cos((-rotation * Math.PI) / 180) * speed);
 
       return Object.assign({}, currentPosition, {
-        x: x + velocityX,
-        y: y + velocityY,
-        velocityX: velocityX * inertia,
-        velocityY: velocityY * inertia,
+        x: x + increasedVelocityX,
+        y: y + increasedVelocityY,
+        velocityX: increasedVelocityX * inertia,
+        velocityY: increasedVelocityY * inertia,
       });
+    }
     default:
       return Object.assign({}, currentPosition, {
         x: x + velocityX,
